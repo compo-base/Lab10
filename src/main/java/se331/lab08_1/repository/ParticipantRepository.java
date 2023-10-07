350de5b1edd63f71fa1ea240177bdbb6065ade97
nureman <nureman_s@cmu.ac.th>
2023-10-07 20:50:03 +0700

    add participant

---
+++ src/main/java/se331/lab08_1/repository/ParticipantRepository.java
@@ -0,0 +1,7 @@
+package se331.lab08_1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se331.lab08_1.entity.Participant;
+
+public interface ParticipantRepository extends JpaRepository<Participant,Long> {
+}
