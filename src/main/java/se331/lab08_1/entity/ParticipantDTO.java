8d96463a37bd369d086fc124f7b707705672d96e
nureman <nureman_s@cmu.ac.th>
2023-10-08 20:48:33 +0700

    fix*

--- src/main/java/se331/lab08_1/entity/ParticipantDTO.java
+++ src/main/java/se331/lab08_1/entity/ParticipantDTO.java
@@ -1,2 +1,18 @@
-package se331.lab08_1.entity;public class ParticipantDTO {
+package se331.lab08_1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipantDTO {
+    Long id;
+    String name;
+    List<EventDTO> events ;
 }
